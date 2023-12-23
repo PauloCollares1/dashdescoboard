9511f6f84af7fb549dd592219a6a5093293dd3df
Paulo Collares <paulocollares1@hotmail.com>
2023-12-23 13:35:55 -0300

    Inserindo o primeiro context

--- app/components/Sidebar/index.tsx
+++ app/components/Sidebar/index.tsx
@@ -1,7 +1,25 @@
+"use client";
 import React from "react";
+// import { useGlobalState } from "@/app/context/globalProvider";
+
+import { useGlobalState } from "@/app/context/globalProvider";
+import styled from "styled-components";
 
 const Sidebar = () => {
-  return <div>sidebar</div>;
+  const { theme } = useGlobalState();
+
+  console.log("theme");
+  console.log(theme);
+
+  return <StyledNavbar theme={theme}>sidebar</StyledNavbar>;
 };
+
+const StyledNavbar = styled.nav`
+  position: relative;
+  border-radius: 1rem;
+  width: ${(props) => props.theme.sidebarWidth};
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+`;
 
 export default Sidebar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Button from "./components/Button";
 
 export default function Home() {
